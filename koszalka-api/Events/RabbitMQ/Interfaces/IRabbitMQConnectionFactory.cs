341b554be5443c348a552a779d66924928ee7227
Raphael Luiz Martins Koszalka <rlkoszalka@latam.stefanini.com>
2023-09-23 09:41:32 -0300

    added rabbitMQConnectionFactory

---
+++ koszalka-api/Events/RabbitMQ/Interfaces/IRabbitMQConnectionFactory.cs
@@ -0,0 +1,9 @@
+﻿using RabbitMQ.Client;
+
+namespace koszalka_api.Events.RabbitMQ.Interfaces
+{
+    public interface IRabbitMQConnectionFactory
+    {
+        IConnection CreateConnection();
+    }
+}
